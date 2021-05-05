11a9a3d7f5704dd867149c6c3299e1b0c67746b4
joeyPark8 <61180930+joeyPark8@users.noreply.github.com>
2021-05-05 23:03:23 +0900

    Add files via upload

--- src/main/java/com/joey/haed/Main.java
+++ src/main/java/com/joey/haed/Main.java
@@ -1,8 +1,6 @@
 package com.joey.haed;
 
-import org.bukkit.ChatColor;
-import org.bukkit.Location;
-import org.bukkit.Material;
+import org.bukkit.*;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandSender;
 import org.bukkit.entity.ArmorStand;
@@ -10,6 +8,7 @@ import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scheduler.BukkitRunnable;
 
 import java.util.*;
 
@@ -30,6 +29,7 @@ public class Main extends JavaPlugin {
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
         Player player = (Player) sender;
         if (command.getName().equalsIgnoreCase("fe")) {
+            //스탠드 소환
             if (args[0].equalsIgnoreCase("spawn")) {
                 Material material = player.getInventory().getItemInMainHand().getType();
                 Location location = player.getEyeLocation();
@@ -40,6 +40,7 @@ public class Main extends JavaPlugin {
                 //stand.setGlowing(true);
                 stand.setVisible(false);
                 stand.setHelmet(stack);
+                stand.setRotation(0, 45);
 
                 stands.put(args[1], stand);
 
@@ -47,6 +48,8 @@ public class Main extends JavaPlugin {
 
                 return true;
             }
+
+            //스탠드 제거
             else if (args[0].equalsIgnoreCase("remove")) {
                 if (args[1].equalsIgnoreCase("all")) {
                     for (ArmorStand i : stands.values()) {
@@ -58,6 +61,16 @@ public class Main extends JavaPlugin {
                 }
 
                 if (stands.containsKey(args[1])) {
+                    if (args.length == 4) {
+                        if (args[2].equalsIgnoreCase("with")) {
+                            if (args[3].equalsIgnoreCase("rocket")) {
+                                World world = stands.get(args[1]).getWorld();
+                                Location location = stands.get(args[1]).getLocation().add(0, stands.get(args[1]).getEyeHeight(), 0);
+
+                                world.spawnParticle(Particle.FIREWORKS_SPARK, location, 50);
+                            }
+                        }
+                    }
                     stands.get(args[1]).remove();
                     stands.remove(args[1]);
                 }
@@ -65,21 +78,146 @@ public class Main extends JavaPlugin {
                     player.sendMessage(ChatColor.RED + "Cannot find stand [" + args[1] + "]");
                 }
             }
+
+            //회전
             else if (args[0].equalsIgnoreCase("rotate")) {
-                if (args[1].equalsIgnoreCase("all")) {
-                    for (ArmorStand i : stands.values()) {
-                        i.setRotation(45,45);
+                if (args[1].equalsIgnoreCase("forever")) {
+                    if  (args[2].equalsIgnoreCase("all")) {
+                        new BukkitRunnable() {
+                            @Override
+                            public void run() {
+                                //todo
+                            }
+                        }.runTaskTimer(this, 0, 2);
                     }
 
-                    return true;
+                    if (stands.containsKey(args[2])) {
+                        final int[] term = {0};
+                        new BukkitRunnable() {
+                            @Override
+                            public void run() {
+                                stands.get(args[2]).setRotation(term[0], 45);
+                                term[0] += Integer.parseInt(args[3]);
+                                if (term[0] >= 360) {
+                                    term[0] = 0;
+                                }
+                            }
+                        }.runTaskTimer(this, 0, 1);
+                    }
+                    else {
+                        player.sendMessage(ChatColor.RED + "Cannot find stand [" + args[2] + "]");
+                    }
                 }
+                else if (args[1].equalsIgnoreCase("degree")) {
+                    if (args[2].equalsIgnoreCase("all")) {
+                        for (ArmorStand i : stands.values()) {
+                            i.setRotation(Integer.parseInt(args[3]),45);
+                        }
 
-                if (stands.containsKey(args[1])) {
-                    stands.get(args[1]).setRotation(Integer.parseInt(args[2]), 45);
+                        return true;
+                    }
+
+                    if (stands.containsKey(args[2])) {
+                        stands.get(args[2]).setRotation(Integer.parseInt(args[3]), 45);
+                    }
+                    else {
+                        player.sendMessage(ChatColor.RED + "Cannot find stand [" + args[1] + "]");
+                    }
                 }
-                else {
-                    player.sendMessage(ChatColor.RED + "Cannot find stand [" + args[1] + "]");
+            }
+
+            //설정
+            else if (args[0].equalsIgnoreCase("setting")) {
+                if (args[2].equalsIgnoreCase("glowing")) {
+                    if (args[3].equalsIgnoreCase("true")) {
+                        if (args[1].equalsIgnoreCase("all")) {
+                            for (ArmorStand i : stands.values()) {
+                                i.setGlowing(true);
+                            }
+                        }
+
+                        if (stands.containsKey(args[1])) {
+                            stands.get(args[1]).setGlowing(true);
+                        }
+                        else {
+                            player.sendMessage(ChatColor.RED + "Cannot find stand [" + args[1] + "]");
+                        }
+                    }
+                    else if (args[3].equalsIgnoreCase("false")) {
+                        if (args[1].equalsIgnoreCase("all")) {
+                            for (ArmorStand i : stands.values()) {
+                                i.setGlowing(false);
+                            }
+                        }
+
+                        if (stands.containsKey(args[1])) {
+                            stands.get(args[1]).setGlowing(false);
+                        }
+                        else {
+                            player.sendMessage(ChatColor.RED + "Cannot find stand [" + args[1] + "]");
+                        }
+                    }
                 }
+                else if (args[2].equalsIgnoreCase("visible")) {
+                    if (args[3].equalsIgnoreCase("true")) {
+                        if (args[1].equalsIgnoreCase("all")) {
+                            for (ArmorStand i : stands.values()) {
+                                i.setVisible(true);
+                            }
+                        }
+
+                        if (stands.containsKey(args[1])) {
+                            stands.get(args[1]).setVisible(true);
+                        }
+                        else {
+                            player.sendMessage(ChatColor.RED + "Cannot find stand [" + args[1] + "]");
+                        }
+                    }
+                    else if (args[3].equalsIgnoreCase("false")) {
+                        if (args[1].equalsIgnoreCase("all")) {
+                            for (ArmorStand i : stands.values()) {
+                                i.setVisible(false);
+                            }
+                        }
+
+                        if (stands.containsKey(args[1])) {
+                            stands.get(args[1]).setVisible(false);
+                        }
+                        else {
+                            player.sendMessage(ChatColor.RED + "Cannot find stand [" + args[1] + "]");
+                        }
+                    }
+                }
+            }
+
+            //도깨비
+            else if (args[0].equalsIgnoreCase("goblin")) {
+                ArmorStand sword = (ArmorStand) player.getWorld().spawnEntity(player.getLocation(), EntityType.ARMOR_STAND);
+                ItemStack stack = new ItemStack(Material.DIAMOND_SWORD);
+
+                sword.setVisible(true);
+                sword.setGravity(false);
+                sword.setItemInHand(stack);
+                sword.getLocation().setDirection(player.getLocation().getDirection());
+
+                stands.put("sword", sword);
+
+                player.sendMessage("날이 좋아서.\n" +
+                        "날이 좋지 않아서.\n" +
+                        "날이 적당해서.\n" +
+                        "모든 날이 좋았다.\n");
+
+                new BukkitRunnable() {
+                    @Override
+                    public void run() {
+                        if (stands.containsKey("sword")) {
+                            sword.getLocation().setDirection(player.getLocation().getDirection());
+                        }
+                        else {
+                            cancel();
+                        }
+                    }
+                }.runTaskTimer(this, 0, 2);
             }
         }
 
@@ -95,17 +233,77 @@ public class Main extends JavaPlugin {
                 modes.add("spawn");
                 modes.add("remove");
                 modes.add("rotate");
+                modes.add("setting");
+                modes.add("goblin");
 
                 return modes;
             }
             if (args.length == 2) {
-                if (args[0].equalsIgnoreCase("remove") || args[0].equalsIgnoreCase("rotate")) {
+                if (args[0].equalsIgnoreCase("remove")) {
                     List<String> targets = new ArrayList<>();
 
                     targets.add("all");
                     targets.addAll(stands.keySet());
 
                     return targets;
+                }
+                else if (args[0].equalsIgnoreCase("rotate")) {
+                    List<String> types = new ArrayList<>();
+
+                    types.add("forever");
+                    types.add("degree");
+
+                    return types;
+                }
+                else if (args[0].equalsIgnoreCase("setting")) {
+                    List<String> targets = new ArrayList<>();
+
+                    targets.add("all");
+                    targets.addAll(stands.keySet());
+
+                    return targets;
+                }
+            }
+            if (args.length == 3) {
+                if (args[0].equalsIgnoreCase("rotate")) {
+                    List<String> targets = new ArrayList<>();
+
+                    targets.add("all");
+                    targets.addAll(stands.keySet());
+
+                    return targets;
+                }
+                else if (args[0].equalsIgnoreCase("setting")) {
+                    List<String> types = new ArrayList<>();
+
+                    types.add("glowing");
+                    types.add("visible");
+
+                    return types;
+                }
+                else if (args[0].equalsIgnoreCase("remove")) {
+                    List<String> extra = new ArrayList<>();
+
+                    extra.add("with");
+
+                    return extra;
+                }
+            }
+            if (args.length == 4) {
+                if (args[0].equalsIgnoreCase("setting")) {
+                    List<String> bool = new ArrayList<>();
+
+                    bool.add("true");
+                    bool.add("false");
+
+                    return bool;
+                }
+                else if (args[0].equalsIgnoreCase("remove")) {
+                    List<String> types = new ArrayList<>();
+
+                    types.add("rocket");
+
+                    return types;
                 }
             }
         }
